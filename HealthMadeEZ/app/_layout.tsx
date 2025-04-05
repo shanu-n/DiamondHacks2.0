fbb5b02c45fa199a0d8c2c8057c05673fad6fbfd
jsandovalortega <jsandovalortega@ucsd.edu>
2025-04-05 15:10:57 -0700

    trying profile screen now

--- HealthMadeEZ/app/_layout.tsx
+++ HealthMadeEZ/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+    <Stack.Screen name="index" options={{ title: 'Home' }} />
+    <Stack.Screen name="profile" options={{ title: 'Profile Screen' }} />
+    </Stack>
+  );
+
 }
--- HealthMadeEZ/app/index.tsx
+++ HealthMadeEZ/app/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View,  StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Health Made EZ</Text>
+      <Link href="/profile" style={styles.button}>
+        Go to Profile screen
+      </Link>
     </View>
   );
 }
@@ -11,6 +16,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    fontSize:30,
     backgroundColor: '#7290b5',
     alignItems: 'center',
     justifyContent: 'center',
@@ -18,4 +24,11 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
   },
+  button: {
+    
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+
 });
